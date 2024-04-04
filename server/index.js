41a922eabfb4f09162c38514be797c6ac0bd568c
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2024-04-04 15:31:54 -0400

    Add User Sign In and Sign Up

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var admin = require("firebase-admin");
 var serviceAccount = require("./firebase-adminsdk.json");
 const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcryptjs");
 const PORT = process.env.PORT || 80;
 
 admin.initializeApp({
@@ -21,13 +22,13 @@ express()
       name: info.name,
       price: info.price,
       description: info.description,
-      pet_type: info.pet_type,
+      petType: info.petType,
       age: info.age,
       imageURL: info.imageURL,
     };
 
-    if (info.pet_subtype) {
-      value["pet_subtype"] = info.pet_subtype;
+    if (info.petSubtype) {
+      value["petSubtype"] = info.petSubtype;
     }
 
     database.ref("items").push(value);
@@ -59,4 +60,144 @@ express()
       }
     }
   })
+  .post("/signIn", async function (req, res) {
+    let info = req.body;
+    let email = info.email;
+    let password = info.password;
+    let returnVal;
+    let myVal = await database
+      .ref("users")
+      .orderByChild("email")
+      .equalTo(email)
+      .once("value");
+    myVal = myVal.val();
+    if (!myVal) {
+      returnVal = {
+        error: true,
+        data: "Incorrect email address.",
+      };
+    } else {
+      let inputPassword = password;
+      let userPassword;
+      for (key in myVal) {
+        userPassword = myVal[key].password;
+      }
+      if (bcrypt.compareSync(inputPassword, userPassword)) {
+        for (key in myVal) {
+          returnVal = {
+            error: false,
+            data: key,
+            firstName: myVal[key].firstName,
+            lastName: myVal[key].lastName,
+            email: email,
+          };
+        }
+      } else {
+        returnVal = {
+          error: true,
+          data: "Incorrect Password",
+        };
+      }
+    }
+    res.send(returnVal);
+  })
+  .post("/signUp", async function (req, res) {
+    let info = req.body;
+    let email = info.email;
+    let firstName = info.firstName;
+    let lastName = info.lastName;
+    let password = info.password;
+    let passwordConfirm = info.passwordConfirm;
+    let returnVal;
+    if (!email) {
+      returnVal = {
+        error: true,
+        data: "Please enter an email address.",
+      };
+      res.send(returnVal);
+      return;
+    }
+    let myVal = await database
+      .ref("users")
+      .orderByChild("email")
+      .equalTo(email)
+      .once("value");
+    myVal = myVal.val();
+    if (myVal) {
+      returnVal = {
+        error: true,
+        data: "Email already exists.",
+      };
+    } else if (firstName.length == 0 || lastName.length == 0) {
+      returnVal = {
+        error: true,
+        data: "Invalid Name",
+      };
+    } else if (
+      !(
+        /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u.test(
+          firstName
+        ) &&
+        /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u.test(
+          lastName
+        )
+      )
+    ) {
+      returnVal = {
+        error: true,
+        data: "Invalid Name",
+      };
+    } else if (
+      !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+        email
+      )
+    ) {
+      returnVal = {
+        error: true,
+        data: "Invalid email address.",
+      };
+    } else if (password.length < 6) {
+      returnVal = {
+        error: true,
+        data: "Your password needs to be at least 6 characters.",
+      };
+    } else if (password != passwordConfirm) {
+      returnVal = {
+        error: true,
+        data: "Your passwords don't match.",
+      };
+    } else {
+      const value = {
+        email: email,
+        password: hash(password),
+        firstName: firstName,
+        lastName: lastName,
+      };
+      database.ref("users").push(value);
+
+      let myVal = await database
+        .ref("users")
+        .orderByChild("email")
+        .equalTo(email)
+        .once("value");
+      myVal = myVal.val();
+
+      for (key in myVal) {
+        returnVal = {
+          error: false,
+          data: key,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        };
+      }
+    }
+    res.send(returnVal);
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function hash(value) {
+  let salt = bcrypt.genSaltSync(10);
+  let hashVal = bcrypt.hashSync(value, salt);
+  return hashVal;
+}
